5ddfa1b4eeb438c424906b4020fa47beda146922
americ <americ@americw>
2017-02-04 14:03:12 +0100

    first commit

---
+++ com.americ.clojure.exercise/doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to com.americ.clojure.exercise
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ com.americ.clojure.exercise/test/com/americ/clojure/exercise_test.clj
@@ -0,0 +1,7 @@
+(ns com.americ.clojure.exercise-test
+  (:require [clojure.test :refer :all]
+            [com.americ.clojure.exercise :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
